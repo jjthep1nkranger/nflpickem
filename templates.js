1bfb0bb3f2221238d94c950cee5afd3f4e7145bc
jimmy annes <Jimmyannes98@gmail.com>
2015-10-28 12:16:46 -0700

    no message

---
+++ templates.js
@@ -0,0 +1,7 @@
+<template name = "hello">
+	<button> Click Me </button> <p> You 've pressed the button {{counter}} times.</p> 
+</template>
+
+<template name = "jimmy">
+	<p> hello and welcome to your eternal misery </p> 
+</template>
